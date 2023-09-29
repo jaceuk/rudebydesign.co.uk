d13bb1c74abc0422fa3ed7d79caeff9a0ccaa604
jaceuk <jnewington@gmail.com>
2023-09-29 21:09:12 +0100

    Removed edit link

--- wp-content/themes/rude-by-design/template-parts/content-page.php
+++ wp-content/themes/rude-by-design/template-parts/content-page.php
@@ -28,26 +28,3 @@
 	);
 	?>
 </div>
-
-<?php if (get_edit_post_link()) : ?>
-	<footer>
-		<?php
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__('Edit <span class="screen-reader-text">%s</span>', 'rude-by-design'),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post(get_the_title())
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-		?>
-	</footer>
-<?php endif; ?>
