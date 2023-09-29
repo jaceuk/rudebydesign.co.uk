cd7f9514d06625420be6ac4cc946f1e3081748bd
jaceuk <jnewington@gmail.com>
2023-09-29 11:45:02 +0100

    Theme

--- wp-content/themes/rude-by-design/home.php
+++ wp-content/themes/rude-by-design/home.php
@@ -17,11 +17,11 @@ do_action('woocommerce_before_main_content');
 
 ?>
 
-<h2 class="section-heading">Best sellers</h2>
+<!-- <h2 class="section-heading">Best sellers</h2>
 
 <?php
 echo do_shortcode('[products limit="4" columns="4" orderby="popularity" best_selling="true" ]');
-?>
+?> -->
 
 <h2 class="section-heading">New arrivals</h2>
 
