4455e30d43715a79f17feebde858bdf1fe1573f3
jaceuk <jnewington@gmail.com>
2023-09-30 12:27:49 +0100

    Hide downloads link from account section

--- wp-content/themes/rude-by-design/inc/woocommerce.php
+++ wp-content/themes/rude-by-design/inc/woocommerce.php
@@ -165,3 +165,11 @@ function woo_remove_product_tabs($tabs)
 	unset($tabs['additional_information']);
 	return $tabs;
 }
+
+// hide downloads link in the account section
+function custom_my_account_menu_items($items)
+{
+	unset($items['downloads']);
+	return $items;
+}
+add_filter('woocommerce_account_menu_items', 'custom_my_account_menu_items');
