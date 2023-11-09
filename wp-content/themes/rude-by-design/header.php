dd39505ed3973f2b71dded6d0727836fb91d0e54
jaceuk <jnewington@gmail.com>
2023-11-09 20:46:47 +0000

    Added FB pixel

--- wp-content/themes/rude-by-design/header.php
+++ wp-content/themes/rude-by-design/header.php
@@ -23,22 +23,50 @@
 	?>
 
 	<?php wp_head(); ?>
+
+	<?php if (!file_exists('c:\windows')) { ?>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-WZLPMWB8YG"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+
+			gtag('config', 'G-WZLPMWB8YG');
+		</script>
+	<?php } ?>
+
+	<!-- Meta Pixel Code -->
+	<script>
+		! function(f, b, e, v, n, t, s) {
+			if (f.fbq) return;
+			n = f.fbq = function() {
+				n.callMethod ?
+					n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+			};
+			if (!f._fbq) f._fbq = n;
+			n.push = n;
+			n.loaded = !0;
+			n.version = '2.0';
+			n.queue = [];
+			t = b.createElement(e);
+			t.async = !0;
+			t.src = v;
+			s = b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t, s)
+		}(window, document, 'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '358964673257456');
+		fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=358964673257456&ev=PageView&noscript=1" /></noscript>
+	<!-- End Meta Pixel Code -->
 </head>
 
-<?php if (!file_exists('c:\windows')) { ?>
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WZLPMWB8YG"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
 
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-
-		gtag('config', 'G-WZLPMWB8YG');
-	</script>
-<?php } ?>
 
 <body <?php body_class(); ?>>
 	<header class="header">
