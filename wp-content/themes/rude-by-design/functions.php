b66087b50908bfd62550dd3cfa0235933037af36
jaceuk <jnewington@gmail.com>
2023-11-14 21:42:13 +0000

    Added FB OG

--- wp-content/themes/rude-by-design/functions.php
+++ wp-content/themes/rude-by-design/functions.php
@@ -133,6 +133,41 @@ if (class_exists('WooCommerce')) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
 
+
+// Facebook compatibility
+function fb_opengraph()
+{
+	global $post;
+
+	if (is_single()) {
+		if (has_post_thumbnail($post->ID)) {
+			$img_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
+		} else {
+			$img_src = get_stylesheet_directory_uri() . '/img/opengraph_image.jpg';
+		}
+		if ($excerpt = $post->post_excerpt) {
+			$excerpt = strip_tags($post->post_excerpt);
+			$excerpt = str_replace("", "'", $excerpt);
+		} else {
+			$excerpt = get_bloginfo('description');
+		}
+?>
+
+		<meta property="og:title" content="<?php echo the_title(); ?>" />
+		<meta property="og:description" content="<?php echo $excerpt; ?>" />
+		<meta property="og:type" content="article" />
+		<meta property="og:url" content="<?php echo the_permalink(); ?>" />
+		<meta property="og:site_name" content="<?php echo get_bloginfo(); ?>" />
+		<meta property="og:image" content="<?php echo $img_src; ?>" />
+
+<?php
+	} else {
+		return;
+	}
+}
+add_action('wp_head', 'fb_opengraph', 5);
+
+
 // change added to cart message
 function custom_add_to_cart_message_html($message, $products)
 {
