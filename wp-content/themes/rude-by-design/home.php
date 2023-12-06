8852daea6a3ec8b00f8935df7d83c9cdf994ce28
jaceuk <jnewington@gmail.com>
2023-12-06 17:41:15 +0000

    Increased number of feartured products

--- wp-content/themes/rude-by-design/home.php
+++ wp-content/themes/rude-by-design/home.php
@@ -20,7 +20,7 @@ do_action('woocommerce_before_main_content');
 <h2 class="section-heading">Featured</h2>
 
 <?php
-echo do_shortcode('[products limit="4" columns="4" orderby="id" order="DESC" visibility="featured"]');
+echo do_shortcode('[products limit="8" columns="4" orderby="id" order="DESC" visibility="featured"]');
 ?>
 
 <h2 class="section-heading">Best sellers</h2>
