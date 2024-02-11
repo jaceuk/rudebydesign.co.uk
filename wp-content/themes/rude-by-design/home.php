502ee16f4fa9f5ea726fc8c6c6713372e2d5b309
jaceuk <jnewington@gmail.com>
2024-02-11 10:55:41 +0000

    Tweaks to support move to tags

--- wp-content/themes/rude-by-design/home.php
+++ wp-content/themes/rude-by-design/home.php
@@ -20,7 +20,7 @@ do_action('woocommerce_before_main_content');
 <h2 class="section-heading">Valentines gifts</h2>
 
 <?php
-echo do_shortcode('[products limit="8" columns="4" orderby="id" order="DESC" category="valentines-mugs"]');
+echo do_shortcode('[products limit="8" columns="4" orderby="id" order="DESC" tag="valentines"]');
 ?>
 
 <h2 class="section-heading">Featured gifts</h2>
