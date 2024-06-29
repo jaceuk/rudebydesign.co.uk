c634a86edf9526deb15c08b0950d24319464414a
jaceuk <jnewington@gmail.com>
2024-06-29 19:31:46 +0100

    Mailpoet

--- wp-content/themes/rude-by-design/footer.php
+++ wp-content/themes/rude-by-design/footer.php
@@ -27,7 +27,7 @@
 			<p>Sign up to our newsletter to stay up to date on new arrivals and get exclusive offers. You can cancel at any time.</p>
 
 			<?php
-			echo do_shortcode('[wpforms id=2369]');
+			echo do_shortcode('[mailpoet_form id="1"]');
 			?>
 		</div>
 
