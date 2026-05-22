aefe10b526b17b4cf47451c423913039748e71d1
jaceuk <jnewington@gmail.com>
2026-05-22 16:13:30 +0100

    Tweaks

--- wp-content/themes/rude-by-design/header.php
+++ wp-content/themes/rude-by-design/header.php
@@ -70,7 +70,7 @@
 
 	<?php
 	$site_url = site_url();
-	if ($site_url === 'http://dev.rudebydesign.co.uk') get_template_part('components/dev-banner');
+	if ($site_url !== 'https://rudebydesign.co.uk') get_template_part('components/dev-banner');
 	?>
 
 	<?php
